2e9f29dc45f6c2df1c7ae28fc2ef760e1cc77bad
mtrupiano <markt6749@gmail.com>
2020-12-18 19:10:56 -0800

    some weather stats presentation

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
         
         var query = $("#search-field").val();
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + query + 
-            "&APPID=4123b80b67c88531547b1bdd29d80fd3";
+            "&units=imperial&APPID=4123b80b67c88531547b1bdd29d80fd3";
         
         $.ajax({url: queryURL, method: "GET"}).then(function(response) {
             // Add new searched city to list in local storage
@@ -39,6 +39,9 @@ $(document).ready(function() {
             markAllInactive();
             newListItem.addClass("list-group-item active");
             $("#search-history-list").prepend(newListItem);
+            loadWeatherData(response);
+
+            console.log(response);
         });
 
     
@@ -47,11 +50,31 @@ $(document).ready(function() {
     $(document).on("click", ".list-group-item", function(event) {
         event.preventDefault();
 
+        // Set clicked list item to 'active'
         var target = $(event.target);
         markAllInactive();
         target.addClass("active");
         var clickedCity = target.text();
+
+        // Get weather data and load it to the appropriate fields
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + clickedCity +
+            "&units=imperial&APPID=4123b80b67c88531547b1bdd29d80fd3";
+        $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
+            console.log(response);
+            loadWeatherData(response);
+        });
     });
+
+    function loadWeatherData(response) {
+        var temp = response.main.temp;
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        var uvIndex;
+
+        $("#temp").text(temp + "degF");
+        $("#humidity").text(humidity + "%");
+        $("#wind-speed").text(windSpeed + " mph");
+    }
 
     // Removes 'active' class from all items in saved search list
     function markAllInactive() {
